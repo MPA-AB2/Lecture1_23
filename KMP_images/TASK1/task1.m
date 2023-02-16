11cff1eb25f502355d7d8901853c4ad20d940379
KMP-img <xpejsk00@vutbr.cz>
2023-02-16 09:51:59 +0100

    PSNR a SSIM Signed-off-by: KMP-img <xpejsk00@vutbr.cz>

--- KMP_images/TASK1/task1.m
+++ KMP_images/TASK1/task1.m
@@ -1,1 +1,17 @@
 clc,clear all,close all
+%% tast1_1
+img_noise = rgb2gray(imread('Lenna_(noisy_image).png'));
+img_org = rgb2gray(imread('Lenna_(original_image).png'));
+
+figure
+subplot(121)
+imshow(img_noise)
+title('noise')
+
+subplot(122)
+imshow(img_org)
+title('org')
+%% task1_2
+PSNR_test = psnr(img_noise,img_org)
+
+SSIM_test = ssim(img_noise,img_org)
