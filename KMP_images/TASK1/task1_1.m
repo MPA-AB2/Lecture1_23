5cbfd4e11f6cfe98f6ebc81413f21f122b04dca6
KMP-img <xpejsk00@vutbr.cz>
2023-02-16 13:26:29 +0100

    tiff Signed-off-by: KMP-img <xpejsk00@vutbr.cz>

--- KMP_images/TASK1/task1_1.m
+++ KMP_images/TASK1/task1_1.m
@@ -19,7 +19,10 @@ obr_gaussian = imgaussfilt(obr_noisy);
 
 psnr_gaussian = psnr(obr_gaussian, obr_orig);
 ssimval_gaussian = ssim(obr_gaussian, obr_orig);
-
+figure
+subplot(241)
+imshow(obr_gaussian)
+title(['psnr = ',num2str(psnr_gaussian),', ','ssim = ', num2str(ssimval_gaussian)])
 %%
 %wieneruv filtr
 noise = abs(obr_orig-obr_noisy);
@@ -35,9 +38,14 @@ for i=1:s-win_size - 1
         obr_wiener(i,j) = ux + (obr_noisy(i,j)-ux)*(vx/(vx+vn));
     end
 end
-obr_wiener=uint8(obr_wiener.*255);
+test=obr_wiener;
+obr_wiener=uint8(obr_wiener);
 psnr_wiener = psnr(obr_wiener, obr_orig(1:s-1,1:d-1));
 ssimval_wiener = ssim(obr_wiener, obr_orig(1:s-1,1:d-1));
+
+subplot(242)
+imshow(obr_wiener)
+title(['psnr = ',num2str(psnr_wiener),', ','ssim = ', num2str(ssimval_wiener)])
 %%
 %adaptive wiener
 adawiener = wiener2(obr_noisy,[5 5]);
@@ -47,6 +55,9 @@ snr_adawiener = psnr(adawiener, obr_orig);
 %SSIM
 ssimval_adawiener = ssim(adawiener, obr_orig);
 
+subplot(243)
+imshow(adawiener)
+title(['psnr = ',num2str(snr_adawiener),', ','ssim = ', num2str(ssimval_adawiener)])
 %%
 %bilateral
 bilateral = imbilatfilt(obr_noisy);
@@ -55,6 +66,10 @@ bilateral = imbilatfilt(obr_noisy);
 snr_bilateral = psnr(bilateral, obr_orig);
 %SSIM
 ssimval_bilateral = ssim(bilateral, obr_orig);
+
+subplot(244)
+imshow(bilateral)
+title(['psnr = ',num2str(snr_bilateral),', ','ssim = ', num2str(ssimval_bilateral)])
 %%
 %nonlinear anisotropic diffusion
 nlad = imdiffusefilt(obr_noisy);
@@ -64,6 +79,9 @@ snr_nlad = psnr(nlad, obr_orig);
 %SSIM
 ssimval_nlad = ssim(nlad, obr_orig);
 
+subplot(245)
+imshow(nlad)
+title(['psnr = ',num2str(snr_nlad),', ','ssim = ', num2str(ssimval_nlad)])
 %%
 %tvl1
 tvl1 = im2double(obr_noisy);
@@ -74,6 +92,10 @@ tvl1 = uint8(tvl1.*255);
 snr_tvl1 = psnr(tvl1, obr_orig);
 %SSIM
 ssimval_tvl1 = ssim(tvl1, obr_orig);
+
+subplot(246)
+imshow(tvl1)
+title(['psnr = ',num2str(snr_tvl1),', ','ssim = ', num2str(ssimval_tvl1)])
 %%
 %non-local means (NLM) filter
 nlmfilt = imnlmfilt(obr_noisy);
@@ -82,3 +104,7 @@ nlmfilt = imnlmfilt(obr_noisy);
 snr_nlm = psnr(nlmfilt, obr_orig);
 %SSIM
 ssimval_nlm = ssim(nlmfilt, obr_orig);
+
+subplot(247)
+imshow(nlmfilt)
+title(['psnr = ',num2str(snr_nlm),', ','ssim = ', num2str(ssimval_nlm)])
